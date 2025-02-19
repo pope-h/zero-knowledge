f8489361bdd918cd34ce8cc2de965093054bce75
pope-h <hamatthan@gmail.com>
2025-02-19 14:57:34 +0100

    fix: ranamed gkr sum check as gkr protocol

--- protocols/src/lib.rs
+++ protocols/src/lib.rs
@@ -2,7 +2,7 @@ pub mod fft;
 pub mod fiat_shamir_non_interactive;
 pub mod gkr_2_to_1_trick;
 pub mod gkr_circuit;
-pub mod gkr_sum_check;
+pub mod gkr_protocol;
 pub mod interactive_sum_check;
 pub mod multi_linear;
 pub mod partial_sum_check;
